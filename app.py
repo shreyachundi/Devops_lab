5d71436540a49f4a91ba4451f4d2ead54999a222
shreyachundi <shreyachundi@gmail.com>
2026-07-20 09:18:45 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,request,render_template
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("register.html")
+
+@app.route('/register',methods=['POST'])
+def register():
+    name=request.form['name']
+    student_id=request.form['student_id']
+    email=request.form['email']
+    year=request.form['year']
+    return render_template(
+        'success.html',
+        name=name,
+        email=email,
+        year=year
+    )
+if __name__=='__main__':
+    app.run(debug=True)
